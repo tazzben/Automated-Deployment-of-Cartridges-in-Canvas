87c685fe400d9f68bf9e6cff80f0b519f89ac021
Ben Smith <ben@bensresearch.com>
2021-06-05 15:26:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ const Canvas = require('./Canvas.js');
 
 const loopCourses = async () => {
     let courses = await Canvas.getCourses();
+    const findMax = (max, b) => {
+        if (max.enrollment_term_id < b.enrollment_term_id){
+            return b;
+        }
+        return max;
+    };
+    let maxTerm = courses.reduce(findMax).enrollment_term_id;
     for (let course of courses){
-        if (course.sis_course_id){
-            console.log(course);
+        if (course.sis_course_id && course.enrollment_term_id == maxTerm){
+            console.log(course.name);
         }
     }
 };
