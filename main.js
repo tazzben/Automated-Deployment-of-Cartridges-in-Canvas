bc98c3b4baf6bf781f012abf1e6b3e05644d8c1e
Ben Smith <ben@bensresearch.com>
2021-06-05 18:55:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const GAPIs = require('./Google.js');
 
 const loopCourses = async () => {
     const courseList = await GAPIs.courseList();
-    console.log(courseList);
     let courses = await Canvas.getCourses();
     const findMax = (max, b) => {
         if (max.enrollment_term_id < b.enrollment_term_id){
@@ -14,7 +13,11 @@ const loopCourses = async () => {
     let maxTerm = courses.reduce(findMax).enrollment_term_id;
     for (let course of courses){
         if (course.sis_course_id && course.enrollment_term_id == maxTerm){
-            console.log(course.sis_course_id);
+            let shortClass = course.sis_course_id.trim().substring(0, 8);
+            let filteredList = courseList.filter(obj => obj.class == shortClass);
+            for (let c of filteredList){
+                console.log(course);
+            }           
         }
     }
 };
