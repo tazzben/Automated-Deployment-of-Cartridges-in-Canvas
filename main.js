477aea4f5e71461c9f001b1651458a226cd68487
Ben Smith <ben@bensresearch.com>
2021-06-09 06:04:33 -0500

    Add AWS region test

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const consoleLoop = require('./Loop.js');
 
-if (!(!!process.env.GCP_PROJECT || !!process.env.FUNCTION_SIGNATURE_TYPE)) {
+if (!(!!process.env.GCP_PROJECT || !!process.env.FUNCTION_SIGNATURE_TYPE || !!process.env.AWS_REGION)) {
     consoleLoop.consoleLoop();
 }
